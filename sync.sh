723c418610f2f098c72e6f127e30ec12a7920110
obobj <vinicolor.violet.end@gmail.com>
2020-07-21 20:21:32 +0800

    1. 添加同步脚本

---
+++ sync.sh
@@ -0,0 +1,5 @@
+git checkout master
+git remote set-url origin https://github.com/spring-projects/spring-boot.git
+git pull
+git remote set-url origin https://github.com/obobj/spring-boot.git
+git push
